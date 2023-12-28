2b9aff5b26e5d496103fc8e09c0474a4e04e9e6d
Min Hong <shmin7777@naver.com>
2023-12-28 16:31:58 +0900

    기본 개발환경 세팅 완료

---
+++ src/main/kotlin/com/example/simpleblog/api/TestController.kt
@@ -0,0 +1,16 @@
+package com.example.simpleblog.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+@RestController
+class TestController(
+
+) {
+    @GetMapping("/health")
+    fun healthTest(): String = "hello kotlin-blog"
+
+
+
+}
